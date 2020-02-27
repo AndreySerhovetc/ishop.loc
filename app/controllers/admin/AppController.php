57619eac5578a1f5fcb9598e95d1b4570c808320
Андрей Серховец <serhovetc88@gmail.com>
2020-02-27 16:53:58 +0200

    add admin panel

---
+++ app/controllers/admin/AppController.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace app\controllers\admin;
+
+
+use ishop\base\Controller;
+
+class AppController extends Controller
+{
+    public  $layout = 'admin';
+}
---
+++ app/controllers/admin/MainController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace app\controllers\admin;
+
+
+class MainController extends AppController
+{
+    public function indexAction(){
+
+    }
+
+}
